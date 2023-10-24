46e196f185229002a23f8b8116f2f8a1ee4b723f
Erik Hoversten <ehoversten@gmail.com>
2023-10-24 15:27:25 -0400

    added formik lib and applied base logic in Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
+import { Formik, useFormik } from 'formik';
 
 function Contact() {
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [city, setCity] = useState('');
-    const [animalType, setAnimalType] = useState(''); // --> "cat" | "dog" | "both" | ""
-    const [animalNum, setAnimalNum] = useState('');
-    const [animalBreed, setAnimalBreed] = useState('');
-    const [animalAge, setAnimalAge] = useState('');
-    const [animalName, setAnimalName] = useState('');
-    const [message, setMessage] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [city, setCity] = useState('');
+    // const [animalType, setAnimalType] = useState(''); // --> "cat" | "dog" | "both" | ""
+    // const [animalNum, setAnimalNum] = useState('');
+    // const [animalBreed, setAnimalBreed] = useState('');
+    // const [animalAge, setAnimalAge] = useState('');
+    // const [animalName, setAnimalName] = useState('');
+    // const [message, setMessage] = useState('');
+    // const [startDate, setStartDate] = useState('');
+    // const [endDate, setEndDate] = useState('');
 
     // const handleChange = (event) => {
     //     console.log(event.target.value);
@@ -29,25 +30,46 @@ function Contact() {
         console.log("Submitted...");
     }
 
+    const formik = useFormik({
+        initialValues: { 
+            firstName: '',
+            lastName: '',
+            email: '',
+            city: '',
+            animalType: '',
+            animalNum: '',
+            animalName: '',
+            animalBreed: '',
+            animalAge: '',
+            message: '',
+            startDate: '',
+            endDate: ''
+        },
+        onSubmit: values => {
+            console.log("Submitting values: ", values);
+        }
+    })
+
   return (
     <div className='contact-container'>
-        <form onSubmit={handleSubmit} className='contact-form'>
+
+        <form onSubmit={formik.handleSubmit} className='contact-form'>
             <label htmlFor='first-name'>First Name</label>
             <input 
                 type='text'
                 id='first-name'
-                name='first-name'
-                value={firstName}
-                onChange={(e => setFirstName(e.target.value))}
+                name='firstName'
+                value={formik.values.firstName}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='last-name'>Last Name</label>
             <input 
                 type='text'
                 id='last-name'
-                name='last-name'
-                value={lastName}
-                onChange={(e => setLastName(e.target.value))}
+                name='lastName'
+                value={formik.values.lastName}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='email'>Contact Email</label>
@@ -55,8 +77,8 @@ function Contact() {
                 type='email'
                 id='email'
                 name='email'
-                value={email}
-                onChange={(e => setEmail(e.target.value))}
+                value={formik.values.email}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='city'>City</label>
@@ -64,12 +86,12 @@ function Contact() {
                 type='text'
                 id='city'
                 name='city'
-                value={city}
-                onChange={(e => setCity(e.target.value))}
+                value={formik.values.city}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor="animal-type">What kind of pet do you have?</label>
-            <select id='animal-type' value={animalType} onChange={(e) => setAnimalType(e.target.value)}>
+            <select id='animal-type' name='animalType' value={formik.values.animalType} onChange={formik.handleChange}>
                 <option value=""> -- Select from option list -- </option>
                 <option value="cat">I have a Cat</option>
                 <option value="dog">I have a Dog</option>
@@ -80,75 +102,75 @@ function Contact() {
             <input 
                 type='checkbox'
                 id='animal-num'
-                name='animal-num'
+                name='animalNum'
                 value='1'
-                onChange={(e => setAnimalNum(e.target.value))}
+                onChange={formik.handleChange}
                 />One
             <input 
                 type='checkbox'
                 id='animal-num'
-                name='animal-num'
+                name='animalNum'
                 value='2'
-                onChange={(e => setAnimalNum(e.target.value))}
+                onChange={formik.handleChange}
                 />Two
             <input 
                 type='checkbox'
                 id='animal-num'
-                name='animal-num'
+                name='animalNum'
                 value='3'
-                onChange={(e => setAnimalNum(e.target.value))}
+                onChange={formik.handleChange}
                 />Three
             <input 
                 type='checkbox'
                 id='animal-num'
-                name='animal-num'
+                name='animalNum'
                 value='4+'
-                onChange={(e => setAnimalNum(e.target.value))}
+                onChange={formik.handleChange}
                 />Four or More
 
             <label htmlFor='animal-breed'>Animal Breed</label>
             <input 
                 type='text'
                 id='animal-breed'
-                name='animal-breed'
-                value={animalBreed}
-                onChange={(e => setAnimalBreed(e.target.value))}
+                name='animalBreed'
+                value={formik.values.animalBreed}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='animal-age'>Animal Age</label>
             <input 
                 type='number'
                 id='animal-age'
-                name='animal-age'
-                value={animalAge}
-                onChange={(e => setAnimalAge(e.target.value))}
+                name='animalAge'
+                value={formik.values.animalAge}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='animal-name'>Name of Pet</label>
             <input 
                 type='text'
                 id='animal-name'
-                name='animal-name'
-                value={animalName}
-                onChange={(e => setAnimalName(e.target.value))}
+                name='animalName'
+                value={formik.values.animalName}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='start-date'>Start Date</label>
             <input 
                 type='date'
                 id='start-date'
-                name='start-date'
-                value={startDate}
-                onChange={(e => setStartDate(e.target.value))}
+                name='startDate'
+                value={formik.values.startDate}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='end-date'>End Date</label>
             <input 
                 type='date'
                 id='end-date'
-                name='end-date'
-                value={endDate}
-                onChange={(e => setEndDate(e.target.value))}
+                name='endDate'
+                value={formik.values.endDate}
+                onChange={formik.handleChange}
                 />
 
             <label htmlFor='message'>Additional Information</label>
@@ -156,12 +178,13 @@ function Contact() {
                 type='text'
                 id='message'
                 name='message'
-                value={message}
-                onChange={(e => setMessage(e.target.value))}
+                value={formik.values.message}
+                onChange={formik.handleChange}
                 />
 
             <button type='submit' className='submit-btn'>Submit Inquiry</button>
         </form>
+
     </div>
   )
 }
