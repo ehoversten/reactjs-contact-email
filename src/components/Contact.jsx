e6653cb18333d05f17e1f883bf9755710a494798
Erik Hoversten <ehoversten@gmail.com>
2023-10-24 15:51:34 -0400

    added formik base level validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,57 @@
 import React from 'react';
 import { useFormik } from 'formik';
 
+const formValidate = values => {
+    const errors = {}
+
+    if(!values.firstName) {
+        errors.firstName = 'Required'
+    }
+    if(!values.lastName) {
+        errors.lastName = 'Required'
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+         errors.email = 'Invalid email address';
+    }
+
+    if(!values.city) {
+        errors.city = 'Required'
+    }
+
+    if(!values.animalType) {
+        errors.animalType = 'Required'
+    }
+
+    if(!values.animalName) {
+        errors.animalName = 'Required'
+    }
+
+    if(!values.animalBreed) {
+        errors.animalBreed = 'Required'
+    }
+    
+    if(!values.animalAge) {
+        errors.animalAge = 'Required'
+    }
+
+    if(!values.animalNum) {
+        errors.animalNum = 'Required'
+    }
+
+    if(!values.startDate) {
+        errors.startDate = 'Required'
+    }
+
+    if(!values.endDate) {
+        errors.endDate = 'Required'
+    }
+
+    return errors
+}
+
 function Contact() {
 
     const formik = useFormik({
@@ -18,10 +69,13 @@ function Contact() {
             startDate: '',
             endDate: ''
         },
+        validate: formValidate,
         onSubmit: values => {
             console.log("Submitting values: ", values);
         }
     })
+
+    // console.log("Form Values: ", formik.values);
 
   return (
     <div className='contact-container'>
@@ -35,6 +89,7 @@ function Contact() {
                 value={formik.values.firstName}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.firstName ? <div className='form-error'>{formik.errors.firstName}</div>: null }
 
             <label htmlFor='last-name'>Last Name</label>
             <input 
@@ -44,6 +99,7 @@ function Contact() {
                 value={formik.values.lastName}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.lastName ? <div className='form-error'>{formik.errors.lastName}</div>: null }
 
             <label htmlFor='email'>Contact Email</label>
             <input 
@@ -53,6 +109,7 @@ function Contact() {
                 value={formik.values.email}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.email ? <div className='form-error'>{formik.errors.email}</div>: null }
 
             <label htmlFor='city'>City</label>
             <input 
@@ -62,6 +119,7 @@ function Contact() {
                 value={formik.values.city}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.city ? <div className='form-error'>{formik.errors.city}</div>: null }
 
             <label htmlFor="animal-type">What kind of pet do you have?</label>
             <select id='animal-type' name='animalType' value={formik.values.animalType} onChange={formik.handleChange}>
@@ -70,6 +128,7 @@ function Contact() {
                 <option value="dog">I have a Dog</option>
                 <option value="both">I have both!</option>
             </select>
+            { formik.errors.animalType ? <div className='form-error'>{formik.errors.animalType}</div>: null }
 
             <label htmlFor='animal-num'>How many pets?</label>
             <input 
@@ -100,6 +159,7 @@ function Contact() {
                 value='4+'
                 onChange={formik.handleChange}
                 />Four or More
+            { formik.errors.animalNum ? <div className='form-error'>{formik.errors.animalNum}</div>: null }
 
             <label htmlFor='animal-breed'>Animal Breed</label>
             <input 
@@ -109,6 +169,7 @@ function Contact() {
                 value={formik.values.animalBreed}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.animalBreed ? <div className='form-error'>{formik.errors.animalBreed}</div>: null }
 
             <label htmlFor='animal-age'>Animal Age</label>
             <input 
@@ -118,6 +179,7 @@ function Contact() {
                 value={formik.values.animalAge}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.animalAge ? <div className='form-error'>{formik.errors.animalAge}</div>: null }
 
             <label htmlFor='animal-name'>Name of Pet</label>
             <input 
@@ -127,6 +189,7 @@ function Contact() {
                 value={formik.values.animalName}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.animalName ? <div className='form-error'>{formik.errors.animalName}</div>: null }
 
             <label htmlFor='start-date'>Start Date</label>
             <input 
@@ -136,6 +199,7 @@ function Contact() {
                 value={formik.values.startDate}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.startDate ? <div className='form-error'>{formik.errors.startDate}</div>: null }
 
             <label htmlFor='end-date'>End Date</label>
             <input 
@@ -145,6 +209,7 @@ function Contact() {
                 value={formik.values.endDate}
                 onChange={formik.handleChange}
                 />
+            { formik.errors.endDate ? <div className='form-error'>{formik.errors.endDate}</div>: null }
 
             <label htmlFor='message'>Additional Information</label>
             <textarea 
